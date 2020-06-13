e44e0c0ba1704d6dc3f14e45453cfa035015f735
hauh <hauh@ya.ru>
2020-06-13 23:29:36 +0300

    testing tests

---
+++ main.py
@@ -0,0 +1,89 @@
+'''Testing module with server to catch requests'''
+
+import logging
+import json
+from threading import Thread
+from queue import Queue, Empty
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from users import UsersCollection
+
+##############################
+
+SERVER_ADDRESS = ('127.0.0.1', 7777)
+REQUESTS_QUEUE = Queue()
+
+logging.basicConfig(format="", level=logging.INFO)
+logger = logging.getLogger('debug')
+
+##############################
+
+
+class BotRequestHandler(BaseHTTPRequestHandler):
+	'''Catches POST requests from bot and puts them to Queue for testing'''
+
+	def log_message(self, *_):
+		pass
+
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length'])
+		post_data = self.rfile.read(content_length)
+		REQUESTS_QUEUE.put(json.loads(post_data, strict=False))
+		self.send_response(200)
+		self.end_headers()
+
+
+def do_test(actions):
+	users_collection = UsersCollection(how_many=10, actions=actions)
+	users_collection.start_actions()
+
+	while True:
+		try:
+			data = REQUESTS_QUEUE.get(timeout=10)
+		except Empty:
+			assert users_collection.is_empty, "Test failed"
+			return
+
+		logger.info("Got data:\n%s", data)
+
+		if not (method := data.get('method', None)):
+			logger.warning('What is that:\n%s', str(data))
+			continue
+
+		if not method == 'sendMessage':
+			continue
+
+		if not (user_id := data['result']['chat']['id']) in users_collection:
+			logger.warning('Who is that:\n%s', str(data))
+			continue
+
+		if not users_collection[user_id].next_action():
+			del users_collection[user_id]
+			logger.info('User %s made it to finish!', user_id)
+		else:
+			logger.info('Next action for user %s', user_id)
+
+
+TEST_TEST = [
+	('message', '/start'),
+	('button', 'matches'),
+	('button', 'profile'),
+	('button', 'set_pubg_id'),
+	('message', lambda self: str(self.user_id)),
+	('buttons', lambda self: f"_confirm_{str(self.user_id)}")
+]
+
+
+def test():
+
+	server = HTTPServer(SERVER_ADDRESS, BotRequestHandler)
+	Thread(target=server.serve_forever).start()
+
+	do_test(TEST_TEST)
+
+	logger.info("Tests done")
+	server.shutdown()
+
+
+if __name__ == '__main__':
+	test()
---
+++ users.py
@@ -0,0 +1,115 @@
+'''Fake user'''
+
+import os
+import random
+import json
+from string import ascii_letters as LETTERS
+
+import requests
+
+##############################
+
+HEADERS = {
+	'Content-Type': "application/json",
+	'Cache-Control': "no-cache"
+}
+BOT_HOOK = os.getenv('BOT_HOOK')
+
+requests.packages.urllib3.disable_warnings()
+
+##############################
+
+
+class User():
+	'''Randomly generated user-like requests'''
+
+	def __init__(self, user_id, actions_iter):
+		self.user_id = user_id
+		self.username = ''.join(random.choice(LETTERS) for _ in range(8))
+		self.actions = actions_iter
+
+	def message(self, text):
+		return {
+			'update_id': 0,
+			'message': {
+				'date': 0,
+				'chat': {
+					'id': self.user_id,
+					'type': "private"
+				},
+				'message_id': 0,
+				'from': {
+					'id': self.user_id,
+					'username': self.username,
+					'first_name': "test_user",
+					'is_bot': False
+				},
+				'text': text
+			}
+		}
+
+	def button(self, text):
+		return {
+			'update_id': 0,
+			'callback_query': {
+				'id': 0,
+				'from': {
+					'id': self.user_id,
+					'username': self.username,
+					'first_name': "test_user",
+					'is_bot': False
+				},
+				'chat_instance': "test",
+				'inline_message_id': 0,
+				'message': {
+					'message_id': 0,
+					'date': 0,
+					'chat': {
+						'id': self.user_id,
+						'type': "private"
+					}
+				},
+				'data': text
+			}
+		}
+
+	def next_action(self):
+		try:
+			method, data = next(self.actions)
+		except StopIteration:
+			return False
+		if callable(data):
+			data = data(self)
+		payload = getattr(self, method)(data)
+		requests.post(
+			BOT_HOOK, headers=HEADERS,
+			verify=False, data=json.dumps(payload)
+		)
+		return True
+
+
+class UsersCollection():
+	'''Generates and holds random users'''
+
+	def __init__(self, *, how_many, actions):
+		self.users = {}
+		for _ in range(how_many):
+			user_id = random.randint(1000000, 9999999)
+			self.users[user_id] = User(user_id, iter(actions))
+
+	def __contains__(self, user_id):
+		return user_id in self.users
+
+	def __getitem__(self, user_id):
+		return self.users.get(user_id, None)
+
+	def __delitem__(self, user_id):
+		del self.users[user_id]
+
+	@property
+	def is_empty(self):
+		return not self.users
+
+	def start_actions(self):
+		for user in self.users.values():
+			user.next_action()
